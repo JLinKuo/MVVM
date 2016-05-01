226b623c9b0c9676e8ba8138915ca7b22bc7104e
heaven <heavenisme@aliyun.com>
2016-05-01 17:10:52 +0800

    delete viewModel

--- app/src/main/java/com/heaven/common/viewModel/Login/ILoginVm.java
+++
@@ -1,9 +0,0 @@
-package com.heaven.common.viewModel.Login;
-
-/**
- * Created by heaven on 2016/4/19.
- */
-public interface ILoginVm {
-    void resCheckCode(String checkCode);
-    void resLoginSuccess(boolean isSuccess);
-}
