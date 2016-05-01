6c3eaeebe031874afc8116e6deed5e54dc84afae
heaven <heavenisme@aliyun.com>
2016-05-01 19:10:04 +0800

    add depend

--- app/src/main/java/com/heaven/common/ui/adapter/CustomAdapter.java
+++ app/src/main/java/com/heaven/common/ui/adapter/CustomAdapter.java
@@ -1,26 +1,93 @@
 package com.heaven.common.ui.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+
+
+import java.util.ArrayList;
 
 /**
  * Created by neusoft on 2016/4/29.
  */
-public class CustomAdapter extends RecyclerView.Adapter{
+public abstract class CustomAdapter<T> extends RecyclerView.Adapter<CustomViewHolder>{
+    private LayoutInflater mInflater = null;
+    private int mItemViewId;
+    private ArrayList<T> mDataList = new ArrayList<>();
 
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public CustomAdapter(Context context, int itemViewId) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mItemViewId = itemViewId;
     }
 
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public CustomAdapter(Context context, int itemViewId, ArrayList<T> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mItemViewId = itemViewId;
+        this.mDataList = data;
+    }
+    /**
+     * dynamic add data
+     */
+    public void appendData(ArrayList<T> appendData) {
+        if (appendData != null && appendData.size() > 0) {
+            mDataList.addAll(appendData);
+            notifyDataSetChanged();
+        }
+    }
 
+    /**
+     * replace all data
+     */
+    public void replaceAllData(ArrayList<T> appendData) {
+        if (appendData != null) {
+            mDataList.addAll(appendData);
+            notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * remove the opposite position data
+     * @param position position
+     */
+    public void remove(int position){
+        if (position >= 0 && position < mDataList.size()) {
+            mDataList.remove(position);
+            notifyDataSetChanged();
+        }
+    }
+    /**
+     * remove all data
+     */
+    public void removeAll(){
+        mDataList.clear();
+        notifyDataSetChanged();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDataList.size();
     }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(mItemViewId,parent);
+        CustomViewHolder holder = new CustomViewHolder(itemView);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder holder, int position) {
+        convert(holder,position);
+    }
+
+    /**
+     * Implement this method and use the helper to adapt the view to the given item.
+     *
+     * @param holder view holder
+     * @param position   the holder position.
+     */
+    protected abstract void convert(CustomViewHolder holder, int position);
+
 }
--- app/src/main/java/com/heaven/common/ui/adapter/CustomViewHolder.java
+++ app/src/main/java/com/heaven/common/ui/adapter/CustomViewHolder.java
@@ -1,9 +1,17 @@
 package com.heaven.common.ui.adapter;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
+import android.text.util.Linkify;
 import android.util.SparseArray;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Checkable;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 /**
@@ -62,6 +70,101 @@ public class CustomViewHolder extends RecyclerView.ViewHolder {
         return CustomViewHolder.this;
     }
 
+    /**
+     * Add an action to set the alpha of a view. Can be called multiple times.
+     * Alpha between 0-1.
+     */
+    public CustomViewHolder setAlpha(int viewId, float value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            getView(viewId).setAlpha(value);
+        } else {
+            // Pre-honeycomb hack to set Alpha value
+            AlphaAnimation alpha = new AlphaAnimation(value, value);
+            alpha.setDuration(0);
+            alpha.setFillAfter(true);
+            getView(viewId).startAnimation(alpha);
+        }
+        return this;
+    }
+
+    /**
+     * Add links into a TextView.
+     *
+     * @param viewId The id of the TextView to linkify.
+     * @return The BaseAdapterHelper for chaining.
+     */
+    public CustomViewHolder linkify(int viewId) {
+        TextView view = getView(viewId);
+        Linkify.addLinks(view, Linkify.ALL);
+        return this;
+    }
+
+    /**
+     * Apply the typeface to the given viewId, and enable subpixel rendering.
+     */
+    public CustomViewHolder setTypeface(int viewId, Typeface typeface) {
+        TextView view = getView(viewId);
+        view.setTypeface(typeface);
+        view.setPaintFlags(view.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+        return this;
+    }
+
+    /**
+     * Sets the progress of a ProgressBar.
+     *
+     * @param viewId   The view id.
+     * @param progress The progress.
+     * @return The BaseAdapterHelper for chaining.
+     */
+    public CustomViewHolder setProgress(int viewId, int progress) {
+        ProgressBar view = getView(viewId);
+        view.setProgress(progress);
+        return this;
+    }
+
+    /**
+     * Sets the progress and max of a ProgressBar.
+     *
+     * @param viewId   The view id.
+     * @param progress The progress.
+     * @param max      The max value of a ProgressBar.
+     * @return The BaseAdapterHelper for chaining.
+     */
+    public CustomViewHolder setProgress(int viewId, int progress, int max) {
+        ProgressBar view = getView(viewId);
+        view.setMax(max);
+        view.setProgress(progress);
+        return this;
+    }
+
+    /**
+     * Sets the rating (the number of stars filled) of a RatingBar.
+     *
+     * @param viewId The view id.
+     * @param rating The rating.
+     * @return The BaseAdapterHelper for chaining.
+     */
+    public CustomViewHolder setRating(int viewId, float rating) {
+        RatingBar view = getView(viewId);
+        view.setRating(rating);
+        return this;
+    }
+
+    /**
+     * Sets the rating (the number of stars filled) and max of a RatingBar.
+     *
+     * @param viewId The view id.
+     * @param rating The rating.
+     * @param max    The range of the RatingBar to 0...max.
+     * @return The BaseAdapterHelper for chaining.
+     */
+    public CustomViewHolder setRating(int viewId, float rating, int max) {
+        RatingBar view = getView(viewId);
+        view.setMax(max);
+        view.setRating(rating);
+        return this;
+    }
+
     public CustomViewHolder setOnClickListener(int viewId, View.OnClickListener listener) {
         View view = getView(viewId);
         view.setOnClickListener(listener);
@@ -85,4 +188,18 @@ public class CustomViewHolder extends RecyclerView.ViewHolder {
         view.setTag(tag);
         return CustomViewHolder.this;
     }
+
+    /**
+     * Sets the checked status of a checkable.
+     *
+     * @param viewId  The view id.
+     * @param checked The checked status;
+     * @return The BaseAdapterHelper for chaining.
+     */
+    public CustomViewHolder setChecked(int viewId, boolean checked) {
+        Checkable view = (Checkable) getView(viewId);
+        view.setChecked(checked);
+        return this;
+    }
+
 }
